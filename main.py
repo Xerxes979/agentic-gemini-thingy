b0c95b57362cd213bb0eed2ef631fa728027da19
Aubrey <aubrey.lego@yahoo.com>
2026-05-09 13:34:15 -0700

    added optional verbose output, argument parsing

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 
 def main():
-    print("Hello from agentic-gemini-thingy!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    # should be able to access args.user_prompt
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise RuntimeError("couldn't get api key")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-3.1-flash-lite", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-    if response.usage_metadata != None:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-    else:
+    response = client.models.generate_content(model="gemini-3.1-flash-lite", contents=messages)
+
+    if response.usage_metadata == None:
         raise RuntimeError("no tokens used, likely failed api request")
-    print(response.text)
+    else:
+        if args.verbose:
+            print("User prompt:", response.text)
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+        else:
+            print(response.text)
 
 
 if __name__ == "__main__":
